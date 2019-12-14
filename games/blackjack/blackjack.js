9f6c13b9949d85465896a9b3b784178e60a93931
LordMussta <mussta1984@gmail.com>
2019-12-14 17:36:38 +1000

    trying to add the images

--- games/blackjack/blackjack.js
+++ games/blackjack/blackjack.js
@@ -1,42 +1,102 @@
-demo = document.getElementById('demo');
+//blackjack.js
+//The javascript file for building my blackjack deck and serving the game
 
+//connect to HTML page
+display = document.getElementById('display');
 
+//set up arrays of the different elements of the playing cards
+let suits = [ 'Hearts', 'Diamonds', 'Spades', 'Clubs' ];
+let cardNumbers = [
+	'Ace',
+	'King',
+	'Queen',
+	'Jack',
+	'Ten',
+	'Nine',
+	'Eight',
+	'Seven',
+	'Six',
+	'Five',
+	'Four',
+	'Three',
+	'Two'
+];
+let deck = [];
+let suitLetters = [ 'H', 'D', 'S', 'C' ];
+let cardNumberLetters = [ 'A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2' ];
+let cardImagesArray = [];
 
-let suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
-let numbers = ["Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"]
-let deck = []
-let deck2 = []
-
-function Card(number, suit) {
-	this.number = number;
-	this.suit = suit;
-	this.background = "BC.jpg";
-	this.displayCard = function() {
-	console.log(this.number + " of " + this.suit);
-	};
-}
-
-let aceHearts = new Card("Ace", "Hearts", 10, "AH.jpg");
-let kingHearts = new Card("King", "Hearts", 10, "KH.jpg");
-let queenHearts = new Card("Queen", "Hearts", 10, "QH.jpg");
-
-let deckArray = []
-
-let testObject = {}
-testObject.myArray = []
-
-
-function forTesting2() {
-	for (let item of suits) {
-		for (let values of numbers) {
-		deckArray.push(new Card(values, item))
+//a function to build the image file references
+function buildCardImageReferences() {
+	for (let item of suitLetters) {
+		for (let value of cardNumberLetters) {
+			cardImagesArray.push('cards/' + value + item + '.jpg');
 		}
 	}
 }
 
-forTesting2()
+buildCardImageReferences();
+console.log(cardImagesArray);
 
-console.log(deckArray.length)
-console.log(deckArray)
+//An object constructor function defines a card in the deck
+function Card(cardNumber, suit, image) {
+	this.cardNumber = cardNumber;
+	this.suit = suit;
+	this.image = image;
+	this.background = 'cards/BC.jpg';
+	this.displayCard = function() {
+		return this.cardNumber + ' of ' + this.suit;
+	};
+}
 
-demo.innerHTML = deckArray[];
+//a function for creating a deck of cards
+function createDeck() {
+	for (let item of suits) {
+		for (let values of cardNumbers) {
+			deck.push(new Card(values, item));
+		}
+	}
+}
+
+//initialise the creation of the deck of cards
+createDeck();
+/*
+function addImages() {
+    for (let filename of cardImagesArray) {
+        deck.push()
+    }
+}
+
+//a function to shuffle the cards in the deck
+function shuffle() {
+	let i = 0;
+	while (51 > deck.length) {
+		var randomNumber = Math.floor(Math.random() * 50);
+		if (deck.length == 50) {
+			break;
+		} else if (deck.includes(randomNumber)) {
+			continue;
+		} else {
+			deck.push(randomNumber);
+		}
+	}
+	return deck;
+}
+
+shuffle();
+*/
+
+//displaying the deck of cards on the HTML page as a list
+for (let index = 0; index < deck.length; index++) {
+	display.innerHTML += '<br>' + (index + 1).toString() + ') ' + JSON.stringify(deck[index].displayCard());
+}
+
+for (let index = 0; index < deck.length; index++) {
+	console.log(deck[index].image);
+}
+
+/*
+let aceHearts = new Card('Ace', 'Hearts', 10, 'AH.jpg');
+let kingHearts = new Card('King', 'Hearts', 10, 'KH.jpg');
+let queenHearts = new Card('Queen', 'Hearts', 10, 'QH.jpg');
+*/
