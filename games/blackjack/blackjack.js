54974660ec2655a2598eff941ff57922dd959f19
Stephen Macey <mussta1984@gmail.com>
2019-12-14 13:06:47 +1000

    getting blackjack working

--- games/blackjack/blackjack.js
+++ games/blackjack/blackjack.js
@@ -1,3 +1,42 @@
 demo = document.getElementById('demo');
 
-demo.innerHTML = 'Try and get a hand that is closer to 21 then the dealer';
+
+
+let suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+let numbers = ["Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"]
+let deck = []
+let deck2 = []
+
+function Card(number, suit) {
+	this.number = number;
+	this.suit = suit;
+	this.background = "BC.jpg";
+	this.displayCard = function() {
+	console.log(this.number + " of " + this.suit);
+	};
+}
+
+let aceHearts = new Card("Ace", "Hearts", 10, "AH.jpg");
+let kingHearts = new Card("King", "Hearts", 10, "KH.jpg");
+let queenHearts = new Card("Queen", "Hearts", 10, "QH.jpg");
+
+let deckArray = []
+
+let testObject = {}
+testObject.myArray = []
+
+
+function forTesting2() {
+	for (let item of suits) {
+		for (let values of numbers) {
+		deckArray.push(new Card(values, item))
+		}
+	}
+}
+
+forTesting2()
+
+console.log(deckArray.length)
+console.log(deckArray)
+
+demo.innerHTML = deckArray[];
